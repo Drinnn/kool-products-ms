679d83449a087e3c665f2b500e7566aa5624401a
Drinnn <pedro_lontro@hotmail.com>
2021-12-22 09:24:16 -0300

    feat: add product "entity"

---
+++ data/products.go
@@ -0,0 +1,35 @@
+package data
+
+import "time"
+
+type Product struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float32
+	SKU         string
+	CreatedAt   string
+	Updatedat   string
+	DeletedAt   string
+}
+
+var productList = []*Product{
+	{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffee",
+		Price:       2.45,
+		SKU:         "abc323",
+		CreatedAt:   time.Now().UTC().String(),
+		Updatedat:   time.Now().UTC().String(),
+	},
+	{
+		ID:          2,
+		Name:        "Esspresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.99,
+		SKU:         "fjd34",
+		CreatedAt:   time.Now().UTC().String(),
+		Updatedat:   time.Now().UTC().String(),
+	},
+}
